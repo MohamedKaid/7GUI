00ffc8f0113ba8f4249a8fbdbf1976376db78b12
MohamedKaid <mohamedkaid014@gmail.com>
2025-03-03 18:17:31 -0500

    added the counter in the routes folder next task will be the temp conver.

---
+++ src/routes/Counter/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = 0;
+</script>
+
+<main class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div class="p-6 bg-white shadow-lg rounded-lg">
+        <button 
+            on:click={() => count++} 
+            class="px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
+        >
+            Clicks: {count}
+        </button>
+    </div>
+</main>
