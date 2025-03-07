5b8b33739ae259c982ea77c294cebb70dbefd54d
MohamedKaid <mohamedkaid014@gmail.com>
2025-03-07 01:02:02 -0500

    updated the routes page.svlete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,13 @@
 
     <!-- Navigation Links -->
     <div class="mt-6 flex flex-col md:flex-row gap-4">
-        <a href="/counter" 
+        <a href="/Counter" 
             class="px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
         >
             ➡ Counter
         </a>
 
-        <a href="/tempconv" 
+        <a href="/TempConv" 
             class="px-6 py-3 bg-green-500 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-green-600 transition duration-300"
         >
             🌡 Temperature Converter
